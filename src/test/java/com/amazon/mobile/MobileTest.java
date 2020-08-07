bcb15f1c947b9da57821ac84f0cd547a45569626
admin <admin@WINCTRL-7M9H3FQ>
2020-08-07 22:09:57 +0530

    demo commit for jenkins run

---
+++ src/test/java/com/amazon/mobile/MobileTest.java
@@ -0,0 +1,28 @@
+package com.amazon.mobile;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+
+	@Test
+	public void samsung() {
+		System.out.println("This is sample samsung Test");
+	}
+	
+	@Test
+	public void lenovo() {
+		System.out.println("This is sample lenovo Test");
+	}
+	@Test
+	public void redmi() {
+		System.out.println("This is sample redmi Test");
+	}
+	@Test
+	public void vivo() {
+		System.out.println("This is sample vivo Test");
+	}
+	
+	
+	
+	
+}
